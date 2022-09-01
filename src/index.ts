2925f42dc40c4146e925380465910a9dcd36c415
Quantum Snowball <quantum.snowball@gmail.com>
2022-09-01 21:42:51 +0800

    feat: all string content in its own file

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import Intro from '../data/Intro'
 import Apps from '../data/Apps'
+import WebDev from '../data/WebDev'
+import AppleDaily from '../data/AppleDaily'
+import Research from '../data/Research'
+import DataScience from '../data/DataScience'
+import Dotfiles from '../data/Dotfiles'
+import Others from '../data/Others'
 import fs from 'fs'
 
 
-const md = Intro + Apps
+const md = Intro + Apps + WebDev + AppleDaily + Research + DataScience + Dotfiles + Others
 fs.writeFile('README.md', md, err => console.log(err))
 
